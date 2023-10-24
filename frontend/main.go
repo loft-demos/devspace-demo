75a443b9be9cf48c9846a94ec770948bf1dca2ae
Kurt Madel <km@kurtmadel.com>
2023-10-24 16:28:18 -0400

    working apps

--- frontend/main.go
+++ frontend/main.go
@@ -3,39 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 func main() {
+	response, error := http.Get("http://api.api.svc.cluster.local:8080")
+	if error != nil {
+	   fmt.Println(error)
+	}
+
+	// read response body
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+	   fmt.Println(error)
+	}
+	// close response body
+	response.Body.Close()
+	
+	API_response := string(body)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, `
 		<html>
 			<head>
-				<title>api-framework</title>
+				<title>auth</title>
 				<link rel="stylesheet" href="https://devspace.sh/css/quickstart.css">
 			</head>
 			<body>
 				<section>
 					<div class="container">
 						<div class="left">
-							<h1>You deployed this project with<img class="logo" src="https://static.loft.sh/branding/logos/devspace/horizontal/mono/devspace_horizontal_mono.svg" alt="DevSpace" /></h1>
-							<h2>Now it's time to code:</h2>
-							<ol>
-								<ul class="dots">
-									<li class="red"></li>
-									<li class="yellow"></li>
-									<li class="blue"></li>
-								</ul>
-								<li>Edit this text in <code>main.go</code> and save the file</li>
-								<li>Restart the application with <code>go run main.go</code></li>
-								<li>Reload browser to see the changes</li>
-							</ol>
+							<h1>api response: %s</h1>
 						</div>
 						<div><img src="https://static.loft.sh/devspace/quickstarts/devspace-astronaut.gif" /></div>
 					</div>
 				</section>
 			</body>
 		</html>
-		`)
+		`, API_response)
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
